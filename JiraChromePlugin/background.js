33b2a75e0281dc6f345f2546f54b5e9625f72150
Spyros <spyrosap@hotmail.com>
2018-12-26 17:29:59 +0100

    Commit mistake

--- JiraChromePlugin/background.js
+++
@@ -1,7 +0,0 @@
-
-// Message Receiver to add a Badge Text with the number of Bloquant Jiras. 
-chrome.runtime.onMessage.addListener(
-  function(message, sender, sendResponse) {
-    chrome.browserAction.setBadgeText({text:message.nbJiraBloquant});
-    chrome.browserAction.setBadgeBackgroundColor({color:"red"})
-});
